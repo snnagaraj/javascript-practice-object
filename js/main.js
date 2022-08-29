dd3e345638820cc1ab794558e683c243804baa5e
nagarajan <nagarajannagarajan670@gmail.com>
2022-08-29 23:58:50 +0530

    add html file

---
+++ js/main.js
@@ -0,0 +1,83 @@
+var mobile={
+    brand:"redmi",
+    model:"note 10s",
+    RAM:"4GB",
+    color:"blue",
+    ROM:"64GB",
+
+}
+console.log(mobile);
+
+var TV={
+    brand:"LG",
+    type:"LED",
+    size:"32inch",
+    price:"10k",
+
+
+}
+console.log(TV);
+
+
+var car={
+    brand:"hundai",
+    model:"i20",
+    fueltype:"petrol",
+    speed:"160",
+    milage:"15km",
+
+
+}
+console.log(car);
+
+var smartwatch={
+    brand:"firebold",
+    model:"mercury",
+    price:"3k",
+    type:"band",
+}
+console.log(smartwatch);
+
+var note={
+    size:"longsize",
+    pages: 150,
+    rate:"30",
+}
+console.log(note);
+
+var pen={
+    type:"ing",
+    color:"blue",
+    length:"4m",
+    rate:"10rs",
+}
+console.log(pen);
+
+var headset={
+    type:"bluetooth",
+    rate:"2k",
+    charge:"500mah",
+}
+console.log(headset);
+
+var profile={
+    name:"nagaraj",
+    age:23,
+    place:"tenkasi",
+}
+console.log(profile);
+
+var waterbottle={
+    size:"1 litter",
+    color:"red",
+    price:"50rs",
+}
+console.log(waterbottle);
+
+var fan={
+    type:"celingfan",
+    seeds:"4",
+    price:"2000",
+    warenty:"1year",
+}
+console.log(fan);
